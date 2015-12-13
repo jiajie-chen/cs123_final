53d4aa186992369b738be2c6d1bb12af7368111e
Jiajie Chen <jiajie_chen@brown.edu>
2015-12-13 07:14:13 -0500

    added texture loading fix without lambdas

--- main.cpp
+++ main.cpp
@@ -13,12 +13,15 @@ int main(int argc, char *argv[])
     // for some reason OpenGL won't load textures until it enters the main event loop
     // we start a qtimer and bind it (with some delay) to run after app.exec() enters the loop
     // syntax uses app.connect to allow binding of lambdas (and thus object member methods)
-    QTimer *timer = new QTimer(&app);
+    /*QTimer *timer = new QTimer(&app);
     timer->setSingleShot(true);
     app.connect(timer, &QTimer::timeout, [&w, timer]() {
         w.fileOpenDefaultScene();
         timer->deleteLater();
     });
-    timer->start(10);
+    timer->start(10);*/
+
+    // this was an alternative fix that avoids lambda (clang/qt w/ clang had issues)
+    QTimer::singleShot(10, &w, SLOT(fileOpenDefaultScene()));
     return app.exec();
 }
